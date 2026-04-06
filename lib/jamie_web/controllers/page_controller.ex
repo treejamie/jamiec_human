ade48e582508acba0a527c850d7ee589a5c5f57a
Jamie Curle <1866+treejamie@users.noreply.github.com>
2026-04-06 12:03:12 +0100

    TRE-12-0.1.4: swarm-hotfixes (#19)
    compose amends to be compatible with swarm

--- lib/jamie_web/controllers/page_controller.ex
+++ lib/jamie_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule JamieWeb.PageController do
   use JamieWeb, :controller
 
-  def home(conn, _params) do
-    render(conn, :home)
+  def health(conn, _params) do
+    conn
+    |> put_root_layout(html: false)
+    |> render(:health)
   end
 end
