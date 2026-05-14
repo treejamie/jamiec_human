fc539f07c627d3961476540aec91f22dff705abb
Jamie Curle <jamie@curle.io>
2026-05-14 13:35:14 +0100

    fix: flashing toast

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,7 @@ import topbar from "../vendor/topbar"
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
+  disconnectedTimeout: 1000,
   params: { _csrf_token: csrfToken },
   hooks: { ...colocatedHooks, SaveShortcut },
 })
