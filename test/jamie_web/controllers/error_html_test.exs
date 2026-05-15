cea3a38aabf3cc5eabf9ba74c5c07742af05bb23
Jamie <1866+jamiecurle@users.noreply.github.com>
2026-05-15 10:39:21 +0100

    404 (#56)
    Now with a 404 page that's lovely.
    
    <img width="872" height="763" alt="image"
    src="https://github.com/user-attachments/assets/c4b515db-a2c2-4615-b992-360129867ff2"
    />

--- test/jamie_web/controllers/error_html_test.exs
+++ test/jamie_web/controllers/error_html_test.exs
@@ -5,7 +5,7 @@ defmodule JamieWeb.ErrorHTMLTest do
   import Phoenix.Template, only: [render_to_string: 4]
 
   test "renders 404.html" do
-    assert render_to_string(JamieWeb.ErrorHTML, "404", "html", []) == "Not Found"
+    assert render_to_string(JamieWeb.ErrorHTML, "404", "html", []) =~ "404"
   end
 
   test "renders 500.html" do
